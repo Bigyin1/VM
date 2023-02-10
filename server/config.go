d7bdc7cc5133802dad48ed3ff1c4b4c4e5e139db
Bigyin1 <sergei.aparin1955@yandex.ru>
2023-02-11 01:30:55 +0300

    refactored cmake lists

--- server/config.go
+++ server/config.go
@@ -10,7 +10,7 @@ var stdlib = []string{
 }
 
 var addr = flag.String("addr", ":8080", "http service address")
-var asmPath = flag.String("asm", "./progs/my_asm", "assermbler path")
+var asmPath = flag.String("asm", "./progs/my_asm", "assermbler path") // TODO: make "better" configuration
 var vmPath = flag.String("vm", "./progs/my_vm", "vm path")
 var ldPath = flag.String("ld", "./progs/my_ld", "ld path")
 var readobjPath = flag.String("readobj", "./progs/readobj", "readobj path")
